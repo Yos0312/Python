3cc97a451069d03f8d09a50be2859eead4e9ecfc
Yos0312 <108702706+Yos0312@users.noreply.github.com>
2022-07-04 19:53:14 -0500

    Primera subida de archivo Hola Mundo

---
+++ python/Hola_Mundo.py
@@ -0,0 +1,16 @@
+# Ejemplo 1. saludo
+#COMENTARIO.-Sirven para documentar o detallar cada parte del código
+#los comentarios los omite o no los toma en cuenta
+
+'''
+cometario: OPERACIONES MATEMATICAS Y SALUDO
+'''
+#imprimir o mostrar resultados en pantalla
+print(2+5)
+print(10-3)
+print(8*7)
+print(40/5)
+
+#imprimir saludo
+print("Hola Mundo")
+print('Jocelyn');
