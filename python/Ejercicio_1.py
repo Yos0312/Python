f83f7f66e7e88c86355859e3117f8dbdd893d75f
Yos0312 <108702706+Yos0312@users.noreply.github.com>
2022-07-05 20:05:19 -0500

    Ejercicio 1

---
+++ python/Ejercicio_1.py
@@ -0,0 +1,18 @@
+# EJERCICIO 1 PROMEDIO 
+
+#ENTRADA DE DATOS
+calificación_1 = float(input("1ra Calificación: "))
+calificación_2 = float(input("2da Calificación: "))
+calificación_3 = float(input("3ra Calificación: "))
+
+
+
+#PROCESOS 
+suma = calificación_1 + calificación_2 + calificación_3
+promedio = suma / 3
+
+
+
+
+#SALIDA DE DATOS 
+print("El promedio es ",round(promedio,2))
