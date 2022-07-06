c169e8127a5d4881b9756ab268c0b9c486ad4ef7
Yos0312 <108702706+Yos0312@users.noreply.github.com>
2022-07-05 20:06:42 -0500

    Ejercicio 3

---
+++ python/Ejercicio_3.py
@@ -0,0 +1,13 @@
+# EJERCICIO 3 EDAD
+
+# ENTRADA DE DATOS
+año_actual = int (input ("Año actual: "))
+año_de_nacimiento = int (input ("Año de nacimiento: "))
+
+
+# PROCESOS
+edad = año_actual - año_de_nacimiento
+
+
+#SALIDA DE DATOS
+print("Su edad actual es de: ", edad)
