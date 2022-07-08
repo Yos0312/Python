f5cc523b9e579b145cc478f410d0120e7194bf76
Yos0312 <108702706+Yos0312@users.noreply.github.com>
2022-07-07 19:22:06 -0500

    Add files via upload

--- python/Ejercicio_1.py
+++ python/Ejercicio_1.py
@@ -10,8 +10,14 @@ calificación_3 = float(input("3ra Calificación: "))
 #PROCESOS 
 suma = calificación_1 + calificación_2 + calificación_3
 promedio = suma / 3
-
-
+if (promedio > 6 and promedio <= 10 ):            # rango >6 y 10
+    print("Aprobado ")
+elif ( promedio == 6 ) :
+    print ("PANSASO")                        #Rango =6
+elif(promedio < 6 and promedio >= 0):                          # rango <6 y >=0
+    print("Reprobado")
+elif(promedio <0 or promedio > 10):    #rango <0 o > 10
+    print("ERROR EN PROMEDIO")
 
 
 #SALIDA DE DATOS 
